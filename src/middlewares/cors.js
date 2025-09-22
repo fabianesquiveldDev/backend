17a826b5b755b84f0e55ce0039c915d49317ff9a
FabianEH <fabian.esquivel.dev@gmail.com>
2025-09-22 12:12:33 -0600

    cambio de cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,22 +1,31 @@
 import cors from 'cors'
+
 const ACCEPTED_ORIGINS = [
-        'http://localhost:5173',
-        'http://localhost:5175',
-        'http://localhost:1234',
-        'https://movies.com',
-        'https://midu.dev'
-    ]
-export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
-    cors({
+    'http://localhost:5173',
+    'http://localhost:5175',
+    'http://localhost:1234',
+    'https://movies.com',
+    'https://midu.dev',
+    'https://app-admin-medicitas-lqciustkl-fabians-projects-e8fa0f2d.vercel.app' 
+]
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
+    const corsOptions = cors({
         origin: (origin, callback) => {
-        if (acceptedOrigins.includes(origin)) {
-            return callback(null, true)
-        }
-
-        if (!origin) {
-            return callback(null, true)
-        }
-
-        return callback(new Error('Not allowed by CORS'))
-        }
+            if (!origin || acceptedOrigins.includes(origin)) {
+                return callback(null, true)
+            }
+            return callback(new Error('Not allowed by CORS'))
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        credentials: true
     })
+
+    return (req, res, next) => {
+        if (req.method === 'OPTIONS') {
+            corsOptions(req, res, next) // manejar preflight
+        } else {
+            corsOptions(req, res, next)
+        }
+    }
+}
